b7139155df9049bbaa2ee2f9bc42a0dcd9fb8e84
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-06-11 00:11:36 -0300

    Implement shift and algorithm

---
+++ algos/shift-and/src/lib.rs
@@ -0,0 +1,311 @@
+use core::Search;
+
+const ALL_ONE: usize = !0;
+
+pub struct ShiftAnd<'t, 'p> {
+    text: &'t [u8],
+    pattern: &'p [u8],
+
+    /// The pattern mask map.
+    pattern_mask: [usize; 1 << u8::BITS],
+    /// The "current bit array".
+    state: usize,
+    /// The cursor.
+    cursor: usize,
+}
+
+impl<'t, 'p> Search<'t, 'p> for ShiftAnd<'t, 'p> {
+    fn new(text: &'t [u8], pattern: &'p [u8]) -> ShiftAnd<'t, 'p> {
+        // For laziness' sake, one doesn't implement the algorithm for patterns
+        // with a length that exceeds the computer's word length.
+        if pattern.len() > usize::BITS as _ {
+            unimplemented!();
+        }
+
+        // Here, one creates the pattern mask map. For example, for the pattern
+        // `AABA`, one'd have the following map:
+        //
+        //   A -> 0100
+        //   B -> 1011
+        //   * -> 1111
+        //
+        // The for loop basically iterates over all occurrences of a given byte-
+        // character in the pattern and, in its corresponding position, `i`,
+        // assigns the bit `0` in reverse. By default, all entries start as on.
+        let mut pattern_mask = [ALL_ONE; 1 << u8::BITS];
+        for (i, char) in pattern.iter().enumerate() {
+            let pos = usize::from(*char);
+            pattern_mask[pos] &= !(1 << i);
+        }
+
+        Self {
+            text,
+            pattern,
+            pattern_mask,
+            state: ALL_ONE,
+            cursor: 0,
+        }
+    }
+
+    fn search(&mut self) -> Option<usize> {
+        if self.pattern.is_empty() {
+            return Some(0);
+        }
+
+        let l = self.pattern.len() - 1; // Len is 1 based; one needs 0 based.
+        let accepting_bit = 1 << l;
+
+        for char in self.text[self.cursor..].iter() {
+            let key = usize::from(*char);
+
+            self.state <<= 1;
+            self.state |= self.pattern_mask[key];
+
+            let old = self.cursor;
+            self.cursor += 1;
+
+            // If the `l`th bit (right to left) is set to zero, then one has
+            // found a match. Return the cursor position.
+            if 0 == (self.state & accepting_bit) {
+                // One must subtract the pattern length since one's at the end.
+                return Some(old - l);
+            }
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_test() {
+        let mut s = ShiftAnd::new(b"abcABCabc", b"A");
+        assert_eq!(s.search().unwrap(), 3);
+
+        let mut s = ShiftAnd::new(b"abcABCabc", b"Z");
+        assert_eq!(s.search(), None);
+    }
+
+    #[test]
+    fn max_pat_length_test() {
+        let pattern = "a".repeat(usize::BITS as _);
+        let text = format!("xx{pattern}yy{pattern}zz{pattern}");
+
+        let mut s = ShiftAnd::new(text.as_bytes(), pattern.as_bytes());
+        assert_eq!(s.search(), Some(2));
+        assert_eq!(s.search(), Some(68));
+        assert_eq!(s.search(), Some(134));
+        assert_eq!(s.search(), None);
+    }
+
+    #[test]
+    fn multiple_occurrences_tests() {
+        let cases = &[
+            ("BABAB", "A", [1, 3].as_slice()),
+            ("XXabcYYabcZZabc", "abc", &[2, 7, 12]),
+        ];
+        for (i, (text, pat, expected_list)) in cases.iter().enumerate() {
+            println!("running test [{i}] for text [{text}] and pattern [{pat}]");
+            let mut s = ShiftAnd::new(text.as_bytes(), pat.as_bytes());
+            for expected in *expected_list {
+                assert_eq!(s.search().unwrap(), *expected);
+            }
+            assert_eq!(s.search(), None);
+        }
+    }
+
+    #[test]
+    fn go_lang_tests() {
+        // Taken from:
+        // https://cs.opensource.google/go/go/+/refs/heads/master:src/strings/strings_test.go;l=45;drc=0b3f58c48e3298e49e27f80dc748f0652339d63e
+        //
+        // (text, pat, expected)
+        let cases = &[
+            ("", "", Some(0)),
+            ("", "a", None),
+            ("", "foo", None),
+            ("fo", "foo", None),
+            ("foo", "foo", Some(0)),
+            ("oofofoofooo", "f", Some(2)),
+            ("oofofoofooo", "foo", Some(4)),
+            ("barfoobarfoo", "foo", Some(3)),
+            ("foo", "", Some(0)),
+            ("foo", "o", Some(1)),
+            ("abcABCabc", "A", Some(3)),
+            (
+                "jrzm6jjhorimglljrea4w3rlgosts0w2gia17hno2td4qd1jz",
+                "jz",
+                Some(47),
+            ),
+            (
+                "ekkuk5oft4eq0ocpacknhwouic1uua46unx12l37nioq9wbpnocqks6",
+                "ks6",
+                Some(52),
+            ),
+            ("999f2xmimunbuyew5vrkla9cpwhmxan8o98ec", "98ec", Some(33)),
+            ("9lpt9r98i04k8bz6c6dsrthb96bhi", "96bhi", Some(24)),
+            (
+                "55u558eqfaod2r2gu42xxsu631xf0zobs5840vl",
+                "5840vl",
+                Some(33),
+            ),
+            // cases with one byte strings - test special case in Index()
+            ("", "a", None),
+            ("x", "a", None),
+            ("x", "x", Some(0)),
+            ("abc", "a", Some(0)),
+            ("abc", "b", Some(1)),
+            ("abc", "c", Some(2)),
+            ("abc", "x", None),
+            // test special cases in Index() for short strings
+            ("", "ab", None),
+            ("bc", "ab", None),
+            ("ab", "ab", Some(0)),
+            ("xab", "ab", Some(1)),
+            (&"xab"[..2], "ab", None),
+            ("", "abc", None),
+            ("xbc", "abc", None),
+            ("abc", "abc", Some(0)),
+            ("xabc", "abc", Some(1)),
+            (&"xabc"[..3], "abc", None),
+            ("xabxc", "abc", None),
+            ("", "abcd", None),
+            ("xbcd", "abcd", None),
+            ("abcd", "abcd", Some(0)),
+            ("xabcd", "abcd", Some(1)),
+            (&"xyabcd"[..5], "abcd", None),
+            ("xbcqq", "abcqq", None),
+            ("abcqq", "abcqq", Some(0)),
+            ("xabcqq", "abcqq", Some(1)),
+            (&"xyabcqq"[..6], "abcqq", None),
+            ("xabxcqq", "abcqq", None),
+            ("xabcqxq", "abcqq", None),
+            ("", "01234567", None),
+            ("32145678", "01234567", None),
+            ("01234567", "01234567", Some(0)),
+            ("x01234567", "01234567", Some(1)),
+            ("x0123456x01234567", "01234567", Some(9)),
+            (&"xx01234567"[..9], "01234567", None),
+            ("", "0123456789", None),
+            ("3214567844", "0123456789", None),
+            ("0123456789", "0123456789", Some(0)),
+            ("x0123456789", "0123456789", Some(1)),
+            ("x012345678x0123456789", "0123456789", Some(11)),
+            (&"xyz0123456789"[..12], "0123456789", None),
+            ("x01234567x89", "0123456789", None),
+            ("", "0123456789012345", None),
+            ("3214567889012345", "0123456789012345", None),
+            ("0123456789012345", "0123456789012345", Some(0)),
+            ("x0123456789012345", "0123456789012345", Some(1)),
+            (
+                "x012345678901234x0123456789012345",
+                "0123456789012345",
+                Some(17),
+            ),
+            ("", "01234567890123456789", None),
+            ("32145678890123456789", "01234567890123456789", None),
+            ("01234567890123456789", "01234567890123456789", Some(0)),
+            ("x01234567890123456789", "01234567890123456789", Some(1)),
+            (
+                "x0123456789012345678x01234567890123456789",
+                "01234567890123456789",
+                Some(21),
+            ),
+            (
+                &"xyz01234567890123456789"[..22],
+                "01234567890123456789",
+                None,
+            ),
+            ("", "0123456789012345678901234567890", None),
+            (
+                "321456788901234567890123456789012345678911",
+                "0123456789012345678901234567890",
+                None,
+            ),
+            (
+                "0123456789012345678901234567890",
+                "0123456789012345678901234567890",
+                Some(0),
+            ),
+            (
+                "x0123456789012345678901234567890",
+                "0123456789012345678901234567890",
+                Some(1),
+            ),
+            (
+                "x012345678901234567890123456789x0123456789012345678901234567890",
+                "0123456789012345678901234567890",
+                Some(32),
+            ),
+            (
+                &"xyz0123456789012345678901234567890"[..33],
+                "0123456789012345678901234567890",
+                None,
+            ),
+            ("", "01234567890123456789012345678901", None),
+            (
+                "32145678890123456789012345678901234567890211",
+                "01234567890123456789012345678901",
+                None,
+            ),
+            (
+                "01234567890123456789012345678901",
+                "01234567890123456789012345678901",
+                Some(0),
+            ),
+            (
+                "x01234567890123456789012345678901",
+                "01234567890123456789012345678901",
+                Some(1),
+            ),
+            (
+                "x0123456789012345678901234567890x01234567890123456789012345678901",
+                "01234567890123456789012345678901",
+                Some(33),
+            ),
+            (
+                &"xyz01234567890123456789012345678901"[..34],
+                "01234567890123456789012345678901",
+                None,
+            ),
+            (
+                "xxxxxx012345678901234567890123456789012345678901234567890123456789012",
+                "012345678901234567890123456789012345678901234567890123456789012",
+                Some(6),
+            ),
+            ("", "0123456789012345678901234567890123456789", None),
+            (
+                "xx012345678901234567890123456789012345678901234567890123456789012",
+                "0123456789012345678901234567890123456789",
+                Some(2),
+            ),
+            (
+                &"xx012345678901234567890123456789012345678901234567890123456789012"[..41],
+                "0123456789012345678901234567890123456789",
+                None,
+            ),
+            (
+                "xx012345678901234567890123456789012345678901234567890123456789012",
+                "0123456789012345678901234567890123456xxx",
+                None,
+            ),
+            (
+                "xx01234567890123456789012345678901234567890123456789012345678\
+                90120123456789012345678901234567890123456xxx",
+                "0123456789012345678901234567890123456xxx",
+                Some(65),
+            ),
+            // test fallback to Rabin-Karp.
+            ("oxoxoxoxoxoxoxoxoxoxoxoy", "oy", Some(22)),
+            ("oxoxoxoxoxoxoxoxoxoxoxox", "oy", None),
+        ];
+        for (i, (text, pat, expected)) in cases.iter().enumerate() {
+            println!("running test [{i}] for text [{text}] and pattern [{pat}]");
+            let mut s = ShiftAnd::new(text.as_bytes(), pat.as_bytes());
+            assert_eq!(s.search(), *expected);
+        }
+    }
+}
